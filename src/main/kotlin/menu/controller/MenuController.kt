8f852ac8c83c26b509d6dd7fc47e5af96d0835f3
dpcks0509 <dpcks0509@gmail.com>
2023-12-14 16:59:10 +0900

    feat: 초기 파일 설정
    점심 메뉴 추천에 필요한 초기 파일들을 설정하였다.

---
+++ src/main/kotlin/menu/controller/MenuController.kt
@@ -0,0 +1,13 @@
+package menu.controller
+
+import menu.view.InputView
+import menu.view.OutputView
+
+class MenuController {
+    private val inputView = InputView()
+    private val outputView = OutputView()
+
+    fun run() {
+
+    }
+}
